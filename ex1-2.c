39d3cbef03bd5d2bffb103afc9935d961a8b5714
Loliver1224 <hi17furukawa@g.kumamoto-nct.ac.jp>
2019-04-15 11:49:28 +0900

    :tada: もう授業受けたくない

---
+++ ex1-2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int factrial(int n){
+    // return n==1 ? 1 : n*factrial(n-1);
+    int ans=1;
+    while(n)
+        ans *= n--;
+    return ans;
+}
+
+int main(void){
+    int n;
+    do{
+        scanf("%d", &n);
+    }while(n<0 || n>31);
+    printf("%d\n", factrial(n));
+    return 0;
+}
