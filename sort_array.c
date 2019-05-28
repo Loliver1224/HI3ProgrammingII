667a3692909e91b89804fc259e0f341e98232043
Loliver1224 <swim.01060808@gmail.com>
2019-05-29 00:13:20 +0900

    :recycle: インデントおかしくなってた

--- sort_array.c
+++ sort_array.c
@@ -19,20 +19,20 @@ void input(dict *data, int n){
 }
 
 void output(dict *data, int n){
-    int i;
+	int i;
 	for(i=0; i<n; i++)
-        printf("%s\t%.1f %.1f\n", data[i].name, data[i].height, data[i].weight);
+		printf("%s\t%.1f %.1f\n", data[i].name, data[i].height, data[i].weight);
 	putchar('\n');
 }
 
 int cmp(const void *a, const void *b){
 	switch(cmd){
-	case 1: return ((dict *)b)->height - ((dict *)a)->height;
-	case 2: return ((dict *)a)->height - ((dict *)b)->height;
-	case 3: return ((dict *)b)->weight - ((dict *)a)->weight;
-	case 4: return ((dict *)a)->weight - ((dict *)b)->weight;
-	case 5: return strcmp(((dict *)a)->name, ((dict *)b)->name);
-	case 6: return strcmp(((dict *)b)->name, ((dict *)a)->name);
+		case 1: return ((dict *)b)->height - ((dict *)a)->height;
+		case 2: return ((dict *)a)->height - ((dict *)b)->height;
+		case 3: return ((dict *)b)->weight - ((dict *)a)->weight;
+		case 4: return ((dict *)a)->weight - ((dict *)b)->weight;
+		case 5: return strcmp(((dict *)a)->name, ((dict *)b)->name);
+		case 6: return strcmp(((dict *)b)->name, ((dict *)a)->name);
 	}
 }
 
@@ -43,24 +43,24 @@ int menu(dict *data, int n){
 	puts("    5:  名前順(昇順)     6:名前順(降順) ");
 	puts("    0:  プログラム終了");
 	puts("================= Sorting Menu =============");
-    reject:
-    printf(">> ");
+	reject:
+	printf(">> ");
 	scanf("%d", &cmd);
 	putchar('\n');
 
 	switch(cmd){
 		case 1: puts("----- 身長を降順に整列 ------");
-		    	sort; break;
+				sort; break;
 		case 2: puts("----- 身長を昇順に整列 ------");
 				sort; break;
 		case 3: puts("----- 体重を降順に整列 ------");
-                sort; break;
+				sort; break;
 		case 4: puts("----- 体重を昇順に整列 ------");
-                sort; break;
-        case 5: puts("----- 名前を昇順に整列 ------");
-                sort; break;
-        case 6: puts("----- 名前を降順に整列 ------");
-                sort; break;
+				sort; break;
+		case 5: puts("----- 名前を昇順に整列 ------");
+				sort; break;
+		case 6: puts("----- 名前を降順に整列 ------");
+				sort; break;
 		case 0: return cmd;
 		default:puts("\a0 ~ 6の数値を入力してください.");
 				goto reject;
@@ -76,7 +76,7 @@ int main(void){
 	putchar('\n');
 	dict data[n];
 
-    puts("データを入力してください（名前 身長 体重）");
+	puts("データを入力してください（名前 身長 体重）");
 	input(data, n);
 
 	while(menu(data, n) != 0);
